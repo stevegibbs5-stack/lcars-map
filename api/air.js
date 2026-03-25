b92a01079e8e150152b4a059c5e36b15f074283e
stevegibbs5-stack <steve.gibbs5@gmail.com>
2026-03-25 14:10:39 +0000

    Update API endpoint and error handling in air.js

--- api/air.js
+++ api/air.js
@@ -1,12 +1,16 @@
 export default async function handler(req, res) {
+  const API_KEY = "4fe7bc94f3e559323d9a467115a14405";
+
   try {
-    const response = await fetch("https://opensky-network.org/api/states/all");
+    const url = `http://api.aviationstack.com/v1/flights?access_key=${API_KEY}&limit=50`;
+
+    const response = await fetch(url);
     const data = await response.json();
 
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).json(data);
 
   } catch (e) {
-    res.status(500).json({ error: "data unavailable" });
+    res.status(500).json({ error: "API failed" });
   }
 }
